47b2171d9457d8178539015e85ea93f9282f8b10
MargaritaVoroncova <margaritavrncv@gmail.com>
2022-08-22 14:56:46 +0300

    task10

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int randomNum=new Random( ). Next (100, 999);
+int remainder = randomNum%100;
+int secondNumber=remainder/10;
+Console.WriteLine($" Вторая цифра числа {randomNum} = {secondNumber}");
